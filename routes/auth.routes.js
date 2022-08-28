3c87f0a5712eaed2bf5b5d122b6338f5d06e7eca
sandeep <sandeepbhartiya9087@gmail.com>
2022-08-28 20:31:48 +0530

    update2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,15 @@
 const authController=require("../controllers/auth.controllers");
 const {authSign}=require("../middelware/index")
 module.exports=(app)=>{
-    app.post("/crm/api/v1/signup",[authSign.signUp],authController.signUp);
-    app.post("/crm/api/v1/signin",[authSign.signIn],authController.signIn)
+
+    /*
+        POST /orderService/api/v1/signup
+    */
+    app.post("/orderService/api/v1/signup",[authSign.signUp],authController.signUp);
+    
+     /*
+        POST /orderService/api/v1/signin
+    */
+    app.post("/orderService/api/v1/signin",[authSign.signIn],authController.signIn)
 
 }
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,8 +1,24 @@
 const orderController=require("../controllers/order.controllers")
 const {authValidation,ordervalid}=require("../middelware/index")
 module.exports=(app)=>{
+    /*
+         POST   /orderService/api/v1/orders
+    */
     app.post("/orderService/api/v1/orders",[ordervalid.orderValidation,authValidation.verifyToken],orderController.placeOrder);
+  
+    /*
+         PUT /orderService/api/v1/orders/{id}
+    */
     app.put("/orderService/api/v1/orders/:id",[ordervalid.orderValidation,authValidation.verifyToken,authValidation.isAdminorOwner],orderController.updateOrder);
+    
+    /*
+         PUT /orderService/api/v1/orders/{id}
+    */
     app.put("/orderService/api/v1/orders/:id",[ordervalid.orderValidation,authValidation.verifyToken,authValidation.isAdminorOwner],orderController.cancelOrder);
+    
+     /*
+        GET /orderService/api/v1/orders/
+    */
+    
     app.get("/orderService/api/v1/orders/",[authValidation.verifyToken],orderController.findAll)
 }
